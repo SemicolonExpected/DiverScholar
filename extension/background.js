e17d05b7ac6b05f0f8e81e02015278f575af2fe5
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2022-02-26 00:23:23 -0500

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -36,7 +36,7 @@ function parseResult() {
     let SERP = document.querySelector("ol.breathe-horizontal"); //get all search results
     //console.log(SERP);
     let results = SERP.children;
-    //console.log(results);
+    console.log(results);
     //console.log(results[i]); //this is in case they change their layout
     let titleAuthorPair = []
     for (var i = 0; i < results.length; i++) {
@@ -44,14 +44,14 @@ function parseResult() {
         let el = results[i].children;
         let title = el[1].innerHTML.trim();
         let el2 = el[2].children;
-        console.log(el2)
         for(var j = 1; j < el2.length; j++){
-            authors.push(el2[j].innerHTML)
+            authors.push(el2[j].innerHTML);
         }
-        titleAuthorPair.push({"title": title, "authors": authors})
+        titleAuthorPair.push({"title": title, "authors": authors});
     }
-    
-    console.log(titleAuthorPair[0]);
+    let url = window.location.href;
+    let output = {"url": url, "entries" : titleAuthorPair};
+    console.log(output);
 
     SERP.innerHTML = "";
 
