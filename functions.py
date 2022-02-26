a640062a973224f9fe72bd81696c2af8adbafc88
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2022-02-26 01:07:41 -0500

    Update functions.py
    allow for batch queries

--- functions.py
+++ functions.py
@@ -4,19 +4,42 @@ Created on Fri Feb 25 21:09:57 2022
 
 @author: SemicolonExpected
 """
-
+import math
 import json
 import requests
 
 def genderize(name):
-    url = "https://api.genderize.io?name="+name
     try:
-        response = requests.get(url, timeout=10)
+        #batch query
+        for i in range(math.floor(len(name)/10)+1):
+            if ((i+1)*10) < len(name): 
+                queryString = '&name[]='.join(name[i*10:(i+1)*10])
+            else:
+                queryString = '&name[]='.join(name[i*10:])
+            url = "https://api.genderize.io?name[]="+queryString
+            try:
+                response = requests.get(url, timeout=10)
+            except:
+                return(-1)
+            if response.status_code == 200:
+                result = json.loads(response.text)
+                [i.pop('count') for i in result]
+                return result
+            else:
+                return(-1)
     except:
-        return(-1)
-    if response.status_code == 200:
-        result = json.loads(response.text)
-        result.pop('count')
-        return result
-    else:
-        return(-1)
+        #single query
+        url = "https://api.genderize.io?name="+name
+        try:
+            response = requests.get(url, timeout=10)
+        except:
+            return(-1)
+        if response.status_code == 200:
+            result = json.loads(response.text)
+            result.pop('count')
+
+            return result
+        else:
+            return(-1)
+
+print(genderize(["peter","lois","stewie","peter","lois","stewie","peter","lois"])) 
