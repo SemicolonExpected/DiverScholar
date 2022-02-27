df0f2278965518d58162ddd84dd9c733f549f36d
walt <walt@danielwalt.io>
2022-02-26 20:45:18 -0500

    finish proper handler with monte carlo rank method; add outputs for the classes of the papers for UI, as well

--- backend/handlers/ranker.go
+++ backend/handlers/ranker.go
@@ -1,15 +1,10 @@
 package handlers
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"google.golang.org/appengine/v2"
-	"google.golang.org/appengine/v2/urlfetch"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 func init() {
@@ -23,7 +18,8 @@ type RankRequest struct {
 }
 
 type RankResponse struct {
-	Ordering []int `json:"ordering,omitempty"`
+	Ordering []int   `json:"ordering,omitempty"`
+	Classes  []Group `json:"classes,omitempty"`
 }
 
 func RankHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,8 +34,6 @@ func RankHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("request: %+v", req)
-
 	// 1. Get User or Add
 	// 2. Check if URL is in cache for User -> return if it is
 	// 3. Check which papers have been scored -> score those that haven't been
@@ -53,42 +47,17 @@ func RankHandler(w http.ResponseWriter, r *http.Request) {
 
 	searchKey, userSearch, err := upsertSearch(ctx, &req, userKey)
 	_ = searchKey
-	_ = userSearch
 	_ = err
 
-	ranks, err := scorePaperCached(ctx, userSearch)
-	_ = ranks
+	assignedGroups, err := scorePapersCached(ctx, userSearch)
 	_ = err
 
-	result, _ := json.Marshal(ranks)
+	var resp RankResponse
+	resp.Ordering = MCRank(assignedGroups)
+	resp.Classes = assignedGroups
+
+	result, _ := json.Marshal(resp)
 	w.Write(result)
 	return
 
-}
-
-func getName(ctx context.Context, first string) *Name {
-
-	// TODO: cache the responses from the name server (also into datastore)
-
-	client := urlfetch.Client(ctx)
-	client.Timeout = time.Millisecond * 200
-
-	resp, err := client.Get(fmt.Sprintf("https://api.genderize.io?name=%s", first))
-	if err != nil {
-		log.Fatal("bad client req to service:", err)
-	}
-	defer resp.Body.Close()
-	if err != nil {
-		log.Fatal("bad http request")
-	}
-
-	respBody, _ := ioutil.ReadAll(resp.Body)
-
-	var result Name
-	err = json.Unmarshal(respBody, &result)
-	if err != nil {
-		log.Fatal("json body err")
-	}
-
-	return &result
 }
